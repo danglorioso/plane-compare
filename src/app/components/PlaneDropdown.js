963c8b63abc18286c6d40051a87f21073908ee85
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2024-12-28 04:52:21 -0500

    Adding comments

--- src/app/components/PlaneDropdown.js
+++ src/app/components/PlaneDropdown.js
@@ -7,6 +7,7 @@ export default function PlaneDropdown({ onPlaneSelect }) {
   useEffect(() => {
     const fetchPlanes = async () => {
       try {
+        // Fetch unique list of planes from the server
         const response = await fetch('/api/fetchPlanes', {
           method: 'GET',
           headers: {
@@ -14,12 +15,15 @@ export default function PlaneDropdown({ onPlaneSelect }) {
           },
         });
 
+        // Throw error if cannot call router handler
         if (!response.ok) throw new Error('Network response was not ok');
 
+        // Parse the JSON response
         const data = await response.json();
 
         // Extract only the full_name (manufacturer + model) for the dropdown
-        setPlanes(data.planes || []); // Assuming 'planes' is an array of tuples
+        // Note: 'planes' is an array of tuples
+        setPlanes(data.planes || []); 
       } catch (error) {
         console.error('Fetch planes error:', error);
       }
@@ -31,6 +35,7 @@ export default function PlaneDropdown({ onPlaneSelect }) {
   return (
     <select onChange={(e) => onPlaneSelect(e.target.value)}>
       <option value="">Select a plane</option>
+      {/* Use each of array element as dropdown option */}
       {planes.map((plane, index) => (
         // Use the ICAO code (plane[0]) as the value
         <option key={index} value={plane[0]}>
