d4fac140af7122b5894e1d3a5bd5bfbc67ab977a
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2024-12-28 19:46:55 -0500

    Homepage instructions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -102,7 +102,7 @@ export default function Home() {
 
 
     <div>
-      <h1 className="text-4xl text-center">Hello World</h1>
+      <h1 className="text-4xl text-center">Go to /compare</h1>
     </div>
   );
 }
